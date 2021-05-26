05714e30fb9c3e32ef9b6b48c4011a5fe4579e8e
jayg98 <jcgar43456@gmail.com>
2021-05-25 23:08:18 -0400

    added our db model

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from models import (Base, session, 
+                    Book, engine)
+
 # import models
 # main menu - add, search, analysis, exit, view
 # add books to DB
@@ -6,3 +9,6 @@
 # search books
 # data cleaning
 # loop runs program
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
--- models.py
+++ models.py
@@ -1,4 +1,25 @@
+from sqlalchemy import (create_engine, Column, 
+                        Integer, String, Date)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///books.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class Book(Base):
+    __tablename__ = 'books'
+    id = Column(Integer, primary_key=True)
+    title = Column('Title', String)
+    author = Column('Author', String)
+    published_date = Column('Published', Date)
+    price = Column('Price', Integer)
+
+    def __repr__(self):
+        return f'Title: {self.title} Author: {self.author} Published: {self.published_date} Price: {self.price}'
 # create a DB
-# books.DD
+# books.DB
 # create a model
 # title, author, date published, price
