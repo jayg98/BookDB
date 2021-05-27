970ca8b14b6475c1193c0492df4d16a4a318cf1f
jayg98 <jcgar43456@gmail.com>
2021-05-27 14:53:20 -0400

    view book function added

--- app.py
+++ app.py
@@ -65,6 +65,28 @@ def clean_price(price_str):
     else:
         return int(price_float * 100)
 
+
+def clean_id(id_str, options):
+    try:
+        book_id = int(id_str)
+    except ValueError:
+        input('''
+                \n*** ID ERROR ***
+                \rThe ID should be a number.
+                \rPress enter to try again
+                \r******************''')
+        return
+    else:
+        if book_id in options:
+            return book_id
+        else:
+            input(f'''
+                \n*** ID ERROR ***
+                \rOptions: {options}.
+                \rPress enter to try again
+                \r******************''')
+            return
+
 def add_csv():
     with open('suggested_books.csv') as csvfile:
         data = csv.reader(csvfile)
@@ -113,7 +135,23 @@ def app():
             input('\nPress enter to return to main menu.')
         elif choice == '3':
             # search books
-            pass
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error = True
+            while id_error:
+                id_choice = input(f'''
+                        \nID Options: {id_options}
+                        \rBook ID: ''')
+                id_choice = clean_id(id_choice, id_options)
+                if type(id_choice) == int:
+                    id_error = False
+            the_book = session.query(Book).filter(Book.id==id_choice).first()
+            print(f'''
+                    \n{the_book.title} by {the_book.author}
+                    \rPublished: {the_book.published_date}
+                    \rPrice: ${the_book.price / 100}''')
+            input('Press enter to return to main menu')
         elif choice == '4':
             # analysis
             pass
@@ -127,7 +165,7 @@ if __name__ == '__main__':
     add_csv()
     app()
     
-    for book in session.query(Book):
-        print(book)
+    #for book in session.query(Book):
+        #print(book)
     
     
